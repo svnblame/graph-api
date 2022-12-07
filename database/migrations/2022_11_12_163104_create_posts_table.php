4de7b6181fbe4c84f65a354076a7bed3117ef980
Gene Kelley <phpcodehead@gmail.com>
2022-12-07 15:37:18 -0700

    Fix Posts migration.

--- database/migrations/2022_11_12_163104_create_posts_table.php
+++ database/migrations/2022_11_12_163104_create_posts_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id('id');
-            $table->unsignedBigInteger('user_id');
             $table->string('title');
             $table->text('body');
             $table->foreignId('user_id',)->constrained();
